be615e8c31f1dc653dacc746ca345253f7230347
gowth08 <gowthampsgtech@gmail.com>
2012-08-07 00:06:54 +0530

    Greedy algorithm for movement of wine

--- gergovia.c
+++ gergovia.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#define MAX 100005
-int arr[MAX];
 int main(){
   int N;
   for(;;){
     scanf("%d", &N);
     if(N == 0) break;
-    int i, j;
-    long long ans = 0;
+    int i, inp;
+    long long ans = 0, bal = 0, cur = 0;
     for(i = 0; i < N; ++i){
-      scanf("%d", &arr[i]);
-    }
-    for(;;){
+      scanf("%d", &inp); // input wine amount
+      cur = inp + bal; // cur is inp + prev wine balance
+      ans += cur < 0 ? -cur : cur; // ans will increase by cur as cur amount has to be moved
+      bal = cur; // set the balance based on cur wine remaining
     }
     printf("%lld\n", ans);
   }
